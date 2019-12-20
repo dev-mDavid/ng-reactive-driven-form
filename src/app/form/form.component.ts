a30611e572a97769657718b522d0aa5bd1f9320a
mDavid <dev.mdavid@gmail.com>
2019-12-20 13:40:11 -0800

    config logic of Form Component Controller

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.sass']
 })
 export class FormComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
-
+  constructor(fb: FormBuilder) { 
+    this.myForm = fb.group({
+      'sku': ['ABC123']
+    });
+  }
+  
   ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
   }
 
 }
